1a697343a693d4f9afbc899ae9b7dd071dc9716c
CharlesHAO <hcheng1005@gmail.com>
2023-12-10 11:32:41 +0800

    update

--- experiments/train.sh
+++ experiments/train.sh
@@ -7,18 +7,18 @@ python main.py \
     ddd \
     --exp_id centerfusion \
     --shuffle_train \
-    --train_split train \
+    --train_split mini_train \
     --val_split mini_val \
     --val_intervals 1 \
     --run_dataset_eval \
     --nuscenes_att \
     --velocity \
-    --batch_size 32 \
+    --batch_size 8 \
     --lr 2.5e-4 \
     --num_epochs 60 \
     --lr_step 50 \
     --save_point 20,40,50 \
-    --gpus 0,1 \
+    --gpus 0 \
     --not_rand_crop \
     --flip 0.5 \
     --shift 0.1 \
@@ -27,7 +27,7 @@ python main.py \
     --pc_z_offset 0.0 \
     --pillar_dims 1.0,0.2,0.2 \
     --max_pc_dist 60.0 \
-    --load_model ../models/centernet_baseline_e170.pth \
+    # --load_model ../models/centernet_baseline_e170.pth \
     # --freeze_backbone \
     # --resume \
 
--- experiments/train_dualRadar.sh
+++ experiments/train_dualRadar.sh
@@ -22,6 +22,7 @@ python mytrain4dual.py \
     --pc_z_offset 0.0 \
     --pillar_dims 1.0,0.2,0.2 \
     --max_pc_dist 60.0 \
+    --freeze_backbone \
     # --load_model ../models/centernet_baseline_e170.pth \
     # --freeze_backbone \
     # --resume \
